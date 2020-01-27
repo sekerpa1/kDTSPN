947b2bc41e8e06e3e4cefb89dc0c368049262571
sekerpa1 <pavol.sekeres8@gmail.com>
2020-01-27 12:51:15 +0100

    added crossover conditions for beginnings of first and second child

--- kdtspn.jl
+++ kdtspn.jl
@@ -9,26 +9,49 @@ struct Individual
 	StartingPoints::Vector{Int32}
 end
 
-function crossover(ind1:Individual, ind2:Individual, point:Int32)
-	if	point > length(ind1.Sequence)
+function crossover(ind1::Individual, ind2::Individual, point::Int32)
+	# check that individual length does not exceed the point of crossover
+	if	point >= length(ind1.Sequence) || (point == length(ind1.Sequence) - 1 && ind2[length] == 1)
 		error("Crossover point outside possible range");
 	end
 	
-	new_ind1 = copy(ind1[1:point]);
-	left_ind1 = copy(ind1[point+1:end]);
-	for i in point+1:length(ind1)
-		
+	# if crossover would cause empty inner sequence at start of ind2 of first child
+	if ind1[point] == 1 && ind2[point+1] == 1
+		#swap two beginning points in individual2
+		swap = ind2[point+1];
+		ind2[point+1] = ind2[point+2];
+		ind2[point+2] = swap;
+	end
+	
+	# if crossover would cause empty inner sequence at start of ind1 of second child
+	if ind2[point] == 1 && ind1[point+1] == 1
+		#swap two beginning points in individual1
+		swap = ind1[point+1];
+		ind1[point+1] = ind1[point+2];
+		ind1[point+2] = swap;
+	end
+	
+	
+	left_ind1 = copy(ind1[1:point]);
+	
+	right_ind1 = copy(ind1[point+1:end]);
+	duplicates_ind1 = [];
+	for i in point+1:length(right_ind1)
+		for j in point+1:length(right_ind1)
+			if(ind2[i] == right_ind1[j])
+				right_ind1[j] = 0;
+			else
+				push!(duplicates, i);
+			end
+		end
+		push!(left_ind1, ind2[i]);
 	end
 	
 	
 end
 
-function getNextIndexNotUsed(arr:Vector{Int32})
-	for i in 1:length(arr)
-		if(arr[i] != 0)
-			return i;
-		end
-	end
+function contains()
+	
 end
 
 function doSomething()
