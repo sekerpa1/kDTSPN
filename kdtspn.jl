3237e33cc4d85572584a4d84a54d13034ceb5538
sekerpa1 <pavol.sekeres8@gmail.com>
2020-01-27 13:49:29 +0100

    changed individual to pure array

--- kdtspn.jl
+++ kdtspn.jl
@@ -4,14 +4,12 @@ module kDTSPN
 
 using Dubins
 
-struct Individual
-	Sequence::Vector{Int32}
-	StartingPoints::Vector{Int32}
-end
-
-function crossover(ind1::Individual, ind2::Individual, point::Int32)
+function crossover(ind1::Vector{Int32}, ind2::Vector{Int32}, point::Int32)
 	# check that individual length does not exceed the point of crossover
-	if	point >= length(ind1.Sequence) || (point == length(ind1.Sequence) - 1 && ind2[length] == 1)
+	if	point >= length(ind1) || 
+		point == length(ind1) - 1 && ind2[length(ind1)] == 1 || 
+		point == length(ind1) - 2 && ind1[length(ind1)-2] == 1 && (ind2[length(ind1) - 1] == 1 || ind2[length(ind1)] == 1) ||
+		
 		error("Crossover point outside possible range");
 	end
 	
@@ -36,29 +34,65 @@ function crossover(ind1::Individual, ind2::Individual, point::Int32)
 	
 	right_ind1 = copy(ind1[point+1:end]);
 	duplicates_ind1 = [];
-	for i in point+1:length(right_ind1)
-		for j in point+1:length(right_ind1)
-			if(ind2[i] == right_ind1[j])
+	
+	for i in point+1:length(ind1)
+		not_found = 1;
+		for j in 1:length(right_ind1)
+			if ind2[i] == right_ind1[j]
 				right_ind1[j] = 0;
-			else
-				push!(duplicates, i);
+				not_found = 0;
+				break;
 			end
+		end
+		if not_found == 1
+			push!(duplicates, i);
 		end
 		push!(left_ind1, ind2[i]);
 	end
 	
+	# replace duplicate in child with available points
+	for i in 1:length(duplicates)
+		for j in 1:length(right_ind1)
+			if (right_ind1[j] != 0 && (right_ind1[j] != 1 || left_ind1[duplicates[i]-1] != 1))
+				left_ind1[duplicates[i]] = right_ind1[j];
+				break
+			end
+		end
+	end
 	
-end
-
-function contains()
+	left_ind2 = copy(ind2[1:point]);
+	right_ind2 = copy(ind2[point+1:end]);
+	duplicates = [];
+	for i in point+1:length(ind1)
+		not_found = 1;
+		for i in 1:length(right_ind2)
+			if(ind1[i] == right_ind2[j])
+				right_ind2[j] = 0;
+				not_found = 0;
+				break;
+			end
+		end
+		if not_found == 1
+			push!(duplicates, i);
+		end
+		push!(left_ind2, ind1[i]);
+	end
+	
+	# replace duplicate in child with available points
+	for i in 1:length(duplicates)
+		for j in 1:length(right_ind2)
+			if (right_ind2[j] != 0 && (right_ind2[j] != 1 || left_ind2[duplicates[i]-1] != 1))
+				left_ind2[duplicates[i]] = right_ind2[j];
+				break;
+			end
+		end
+	end
+	
+	return [left_ind1, left_ind2];
 	
 end
 
 function doSomething()
-	a = Individual([3,4], [3,5]);
-	#a.Sequence = [3,4];
-	#a.StartingPoints = [3,5];
-	println(a.Sequence, a.StartingPoints)
 end
 
 doSomething();
